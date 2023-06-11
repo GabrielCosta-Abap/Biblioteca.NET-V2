e1a602820ee9175fd4b876ae33f59532e8aaa063
GabrielCosta-Abap <gabriel.costarosa.9@gmail.com>
2023-06-11 17:56:42 -0300

    Finalização do CRUD de autores

--- Biblioteca/Models/Autor.cs
+++ Biblioteca/Models/Autor.cs
@@ -1,9 +1,15 @@
-﻿namespace Biblioteca.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblioteca.Models
 {
     public class Autor
     {
         public int Id { get; set; }
+
+        [Display(Name = "Nome do autor")]
         public string NomeAutor { get; set; }
+
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
     }
 }
