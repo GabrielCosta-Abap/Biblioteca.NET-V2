302d455c64ac0cca81df94a5d1ccbcafd6e126e2
LeonardoRockstroh <leonardorockstroh@gmail.com>
2023-06-11 18:04:33 -0300

    Cliente

---
+++ Biblioteca/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿namespace Biblioteca.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string NomeCliente { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string Cpf { get; set; }
+        public string StatusAtivo { get; set; }
+    }
+}
