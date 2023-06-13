9b02d75a12b48feeb0803990bfdae9d8cfbf78c9
leonardoRockstroh <leonardorockstroh@gmail.com>
2023-06-13 17:12:25 -0300

    Livros

---
+++ Biblioteca/Controllers/LivrosController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Biblioteca.Data;
+using Biblioteca.Models;
+using Biblioteca.Models.ViewModels;
+using NuGet.Protocol.Plugins;
+using System.Net;
+
+namespace Biblioteca.Controllers
+{
+    public class LivrosController : Controller
+    {
+        private readonly BibliotecaContext _context;
+
+        public LivrosController(BibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Livros
+        public async Task<IActionResult> Index()
+        {
+              return _context.Livro != null ? 
+                          View(await _context.Livro.ToListAsync()) :
+                          Problem("Entity set 'BibliotecaContext.Livro'  is null.");
+        }
+
+        // GET: Livros/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Livro == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return View(livro);
+        }
+
+        // GET: Livros/Create
+        public IActionResult Create()
+        {
+            LivroFromViewModels livroViewModel = new LivroFromViewModels();
+            livroViewModel.Autors = _context.Autor.ToList();
+            return View(livroViewModel);
+        }
+
+        // POST: Livros/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Livro livro)
+        {
+            _context.Add(livro);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Livros/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Livro == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livro.FindAsync(id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+            return View(livro);
+        }
+
+        // POST: Livros/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NomeLivro,IdAutor,Editora,Ano,tema,NumVolume,QtdVolumes,ValorLocacao")] Livro livro)
+        {
+            if (id != livro.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(livro);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LivroExists(livro.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(livro);
+        }
+
+        // GET: Livros/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Livro == null)
+            {
+                return NotFound();
+            }
+
+            var livro = await _context.Livro
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (livro == null)
+            {
+                return NotFound();
+            }
+
+            return View(livro);
+        }
+
+        // POST: Livros/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Livro == null)
+            {
+                return Problem("Entity set 'BibliotecaContext.Livro'  is null.");
+            }
+            var livro = await _context.Livro.FindAsync(id);
+            if (livro != null)
+            {
+                _context.Livro.Remove(livro);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LivroExists(int id)
+        {
+          return (_context.Livro?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Biblioteca/Models/Livro.cs
@@ -0,0 +1,18 @@
+﻿using NuGet.Protocol.Plugins;
+
+namespace Biblioteca.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string NomeLivro { get; set; }
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; }
+        public string Editora { get; set; }
+        public int Ano { get; set; }
+        public string tema { get; set; }
+        public int NumVolume { get; set; }
+        public int QtdVolumes { get; set; }
+        public double ValorLocacao { get; set; }
+    }
+}
