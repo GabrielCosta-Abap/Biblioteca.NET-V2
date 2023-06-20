e8e8eb219f4c224e610e61ef6652e946e5443f69
GabrielCosta-Abap <gabriel.costarosa.9@gmail.com>
2023-06-19 21:35:45 -0300

    Criação da classe locação com suas annotations

---
+++ Biblioteca/Models/ViewModels/LocacaoFromViewModels.cs
@@ -0,0 +1,10 @@
+﻿namespace Biblioteca.Models.ViewModels
+{
+    public class LocacaoFromViewModels
+    {
+        public Locacao Locacao { get; set; }
+        public ICollection<Livro> Livros { get; set; }
+        public ICollection<Cliente> Clientes { get; set; }
+        public ICollection<Usuario> Usuarios { get; set; }
+    }
+}
